fd5483725b219a67e802eb91e458220b801c6b63
frrp9 <121900262+frrp9@users.noreply.github.com>
2023-06-20 14:52:39 +0000

    change place all

---
+++ script.js
@@ -0,0 +1,10 @@
+function closePopup() {
+    var popup = document.getElementById("popup");
+    popup.style.display = "none";
+  }
+  
+  function acceptCookies() {
+    var cookieBanner = document.getElementById("cookie-banner");
+    cookieBanner.style.display = "none";
+  }
+  
